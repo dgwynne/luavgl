4181f6d41c9e41ddad7713551c1034cd58292ea3
David Gwynne <david@gwynne.id.au>
2023-09-25 11:00:24 +1000

    add a method for sending EVENT_CHANGED when the value gets changed

--- src/widgets/switch.c
+++ src/widgets/switch.c
@@ -7,7 +7,17 @@ luavgl_switch_create(lua_State *L)
 	return luavgl_obj_create_helper(L, lv_switch_create);
 }
 
+static int
+luavgl_switch_value_changed(lua_State *L)
+{
+	lv_obj_t *obj = luavgl_to_obj(L, 1);
+	lv_event_send(obj, LV_EVENT_VALUE_CHANGED, NULL);
+	return 0;
+}
+
 static const luaL_Reg luavgl_switch_methods[] = {
+	{ "valueChanged",	luavgl_switch_value_changed },
+
 	{ NULL, NULL }
 };
 
