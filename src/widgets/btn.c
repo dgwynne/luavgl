84fb0c48ffacbe9a09aab60d46790fb9649d5904
David Gwynne <david@gwynne.id.au>
2023-09-25 15:06:57 +1000

    add valueChanged (from switches)

--- src/widgets/btn.c
+++ src/widgets/btn.c
@@ -7,8 +7,18 @@ luavgl_btn_create(lua_State *L)
 	return luavgl_obj_create_helper(L, lv_btn_create);
 }
 
+static int
+luavgl_btn_value_changed(lua_State *L)
+{
+	lv_obj_t *obj = luavgl_to_obj(L, 1);
+	lv_event_send(obj, LV_EVENT_VALUE_CHANGED, NULL);
+	return 0;
+}
+
 static const luaL_Reg luavgl_btn_methods[] = {
-	{ NULL,		NULL }
+	{ "valueChanged",	luavgl_btn_value_changed },
+
+	{ NULL,			NULL }
 };
 
 static void
